a38c67b52392d1927359d6a124f2fdc813932db0
Axel Hamilcaro <68954111+axelhamil@users.noreply.github.com>
2021-04-29 17:45:28 +0200

    Feature/all courses page (#6)
    * Added Card to display all courses
    
    * added page with All courses
    
    * fix error in App.test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
+import { AllCoursesPage } from "./components/AllCoursesPage";
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <section>
+          <Switch>
+            <Route path="/cours">
+              <AllCoursesPage />
+            </Route>
+            <Route path="/:categories/:id">
+              <h1>Hello World!</h1>
+            </Route>
+          </Switch>
+        </section>
+      </div>
+    </Router>
   );
 }
 
