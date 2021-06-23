9e178935e9628b185fb44be6dd4a91a7b3ba11b6
Axel Hamilcaro <68954111+axelhamil@users.noreply.github.com>
2021-06-23 17:04:00 +0200

    added dynamic form for course

---
+++ src/components/CourseForm/courseFormReducer.js
@@ -0,0 +1,83 @@
+export const initialState = {
+  title: "",
+  video: "",
+  categories: "",
+  description: "",
+  doc: {
+    0: {
+      linkTitle: "",
+      linkUrl: "",
+      imgUrl: "",
+    },
+  },
+};
+
+export const courseFormReducer = (state, action) => {
+  switch (action.type) {
+    case "addField":
+      return addField(state, action);
+    case "addContent":
+      return addContent(state, action);
+    case "addDoc":
+      return addDoc(state, action);
+    case "reset":
+      // eslint-disable-next-line no-case-declarations
+      const copiedState = { ...state };
+      copiedState.length = 0;
+      return { ...copiedState };
+
+    default:
+      throw Error("Problème d'Ajout");
+  }
+};
+
+const addField = (state, action) => {
+  const lastElement = Object.keys(state.doc || {}).length;
+  const newState = {
+    ...state,
+    doc: {
+      ...state?.doc,
+      [lastElement]: {
+        ...action?.doc,
+      },
+    },
+  };
+  return { ...newState };
+};
+
+const addContent = (state, action) => {
+  const newState = {
+    ...state,
+    ...(Object.keys(action).includes("title") && { title: action?.title }),
+    ...(Object.keys(action).includes("video") && { video: action?.video }),
+    ...(Object.keys(action).includes("categories") && {
+      categories: action?.categories,
+    }),
+    ...(Object.keys(action).includes("description") && {
+      description: action?.description,
+    }),
+  };
+  return { ...newState };
+};
+
+const addDoc = (state, action) => {
+  const newState = {
+    ...state,
+    doc: {
+      ...state?.doc,
+      [action.index]: {
+        ...state?.doc[action?.index],
+        ...(Object.keys(action).includes("linkTitle") && {
+          linkTitle: action?.linkTitle,
+        }),
+        ...(Object.keys(action).includes("linkUrl") && {
+          linkUrl: action?.linkUrl,
+        }),
+        ...(Object.keys(action).includes("imgUrl") && {
+          imgUrl: action?.imgUrl,
+        }),
+      },
+    },
+  };
+  return { ...newState };
+};
